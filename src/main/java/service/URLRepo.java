ec6ec4876a3d92636bc78c35821c162b823bff80
Guillaume <guillaume.normand@epita.fr>
2017-12-06 15:10:52 +0100

    add URLRepo

--- src/main/java/service/URLRepo.java
+++ src/main/java/service/URLRepo.java
@@ -2,23 +2,44 @@ package service;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by Guillaume on 06/12/2017.
  */
 public class URLRepo implements IURLRepo {
 
+    private final Stack<URL> notCrawledUrl;
+    private final Stack<URL> crawledUrl;
 
+    public Stack<URL> getNotCrawledUrl() {
+        return notCrawledUrl;
+    }
+
+    public Stack<URL> getCrawledUrl() {
+        return crawledUrl;
+    }
+
+    public URLRepo() {
+        this.notCrawledUrl = new Stack<URL>();
+        this.crawledUrl = new Stack<URL>();
+    }
 
     public void receive(final List<URL> urls) {
-
+        //
     }
 
     public void store(final List<URL> urls) {
-
+        for (URL url : urls) {
+            if (!notCrawledUrl.contains(url))
+                notCrawledUrl.add(url);
+        }
     }
 
     public URL request() {
-        return null;
+
+        URL url = notCrawledUrl.pop();
+        crawledUrl.add(url);
+        return url;
     }
 }
