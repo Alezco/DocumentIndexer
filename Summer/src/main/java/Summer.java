ae94d3dc3ca7fb6a297d4e758fcc8db182a4f979
Guillaume <guillaume.normand@epita.fr>
2017-12-07 20:41:02 +0100

    update summer

--- Summer/src/main/java/Summer.java
+++ Summer/src/main/java/Summer.java
@@ -1,25 +1,29 @@
-import scope.MyScope;
+import scope.AnyScope;
 
 import java.util.Stack;
 
 public class Summer {
 
-    public final Stack<MyScope> scopeStack;
+    public final Stack<AnyScope> scopeStack;
 
     public Summer() {
         this.scopeStack = new Stack<>();
-        scopeStack.push(new MyScope());
+        this.scopeStack.push(new AnyScope());
     }
 
     public void bean(Class c, Object obj) {
-        scopeStack.peek().create(c, obj);
+        this.scopeStack.peek().create(c, obj);
     }
 
     public Object instanceOf(Class c) {
-        for (MyScope scope : scopeStack) {
+        for (AnyScope scope : scopeStack) {
             if (scope.get(c) != null)
                 return scope.get(c).get(c);
         }
         return null;
+    }
+
+    public void removeScope() {
+        this.scopeStack.pop();
     }
 }
--- Summer/src/main/java/scope/AnyScope.java
+++ Summer/src/main/java/scope/AnyScope.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * Created by Guillaume on 07/12/2017.
  */
-public class MyScope implements Scope  {
+public class AnyScope implements Scope  {
 
     private List<Provider> providerList;
 
