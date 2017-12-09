44bd28a1a14ebdfdd61d460eaa4bd710cc952f0b
Benjamin Morali <morali_b@epita.fr>
2017-12-09 10:21:17 +0100

    Add ExecutionContext

--- Summer/src/main/java/aspect/ExecutionContext.java
+++ Summer/src/main/java/aspect/ExecutionContext.java
@@ -1,10 +1,27 @@
 package aspect;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class ExecutionContext implements Aspect {
-    @Override
-    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+public class ExecutionContext {
+    private final Object proxy;
+    private final Method method;
+    private final Object[] args;
+    private final Object object;
+
+    public ExecutionContext(final Object proxy, final Method method, final Object[] args, final Object object) {
+        this.proxy = proxy;
+        this.method = method;
+        this.args = args;
+        this.object = object;
+    }
+
+    Object proceed() {
+        try {
+            return method.invoke(object, args);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
